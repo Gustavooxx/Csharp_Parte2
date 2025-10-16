3f0237a774fb7b7a8f6d39437101cead3deabec5
Gustavooxx <gustaviixx@gmail.com>
2025-10-16 09:29:38 -0300

    subindo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Csharp_Parte2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a cor do semáforo:");
+        string cor = Console.ReadLine().ToLower();
+        string acao = GetAction(cor);
+        Console.WriteLine(acao);
+    }
+
+    static string GetAction(string cor)
+    {
+        var coresAcoes = new (string cor, string acao)[]
+        {
+            ("verde", "atravesse"),
+            ("vermelho", "espere")
+        };
+
+        for (int i = 0; i < coresAcoes.Length; i++)
+        {
+            if (coresAcoes[i].cor == cor)
+            {
+                return coresAcoes[i].acao;
+            }
+        }
+
+        return "Farol inoperante";
+    }
+}
