fd19e80d296730d4582953e9d7fc5f4994c9f8cd
Gustavooxx <gustaviixx@gmail.com>
2025-10-18 22:55:14 -0300

    novo exer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 
 namespace Csharp_Parte2;
 
@@ -6,42 +6,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Apresentacao();
-        int salario = pegarSalario();
-        int gastos = pegarGastos();
-        string resultado = Resu(salario, gastos);
-        Console.WriteLine(resultado);
+        Console.WriteLine("Informe o número do dia da semana (0-6):");
+        int dia = Convert.ToInt32(Console.ReadLine());
+
+        string[] diasSemana = { "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado" };
+
+        if (dia >= 0 && dia <= 6)
+        {
+            Console.WriteLine($"O dia da semana é: {diasSemana[dia]}");
+        }
+        else
+        {
+            Console.WriteLine("Dia da semana inválido");
+        }
     }
 
     public static void Apresentacao()
     {
-        Console.WriteLine("PROGRAMA DO ORCAMENTO");
+        Console.WriteLine("PROGRAMA DA SEMANA ");
     }
-
-    public static int pegarSalario()
-    {
-        Console.WriteLine("Informe o salario:");
-        int salario = Convert.ToInt32(Console.ReadLine());
-        return salario;
-    }
-
-    public static int pegarGastos()
-    {
-        Console.WriteLine("Informe os gastos:");
-        int gastos = Convert.ToInt32( Console.ReadLine());
-        return gastos;
-    }
-    public static string Resu(int salario, int gastos)
-    {
-        int orcamento = salario - gastos;
-        if (orcamento >= 0)
-        {
-            return "Você está dentro do orçamento!";
-        }
-        else
-        {
-            return "Você está fora do orçamento! Não gaste mais!";
-        }
-    }
-    
 }
